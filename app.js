82d909005b81acab51747a8dccd34e0102df2021
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2017-10-05 09:17:33 +0100

    [feature] School Information System

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //importing modules
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var path = require('path');
+
+
+var app = express();
+
+
+//port no
+const port = 3000;
+
+//testing server
+app.get('/',function (req, res) {
+   res.send('Me');
+});
+
+app.listen(port,function () {
+     console.log('Server started at port'+port);
+});

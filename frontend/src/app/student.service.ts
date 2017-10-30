98809cfffb78165a9b6621c7fa8164d9c20c5553
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2017-10-30 11:12:23 +0100

    [feature] sms2loan

---
+++ frontend/src/app/student.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+  import { Student } from './student';
+  import 'rxjs/add/operator/map';
+
+  @Injectable()
+  export class StudentService {
+
+  constructor(private http: Http) { }
+    // retrieving students
+    getStudents() {
+      return this.http.get('api/students')
+        .map(res => res.json());
+    }
+
+    // retrieve one student
+    getOneStudent(id) {
+      return this.http.get('api/student/' + id)
+        .map(res => res.json());
+    }
+
+    // add student
+  addStudent(newStudent) {
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      return this.http.post('api/student', newStudent, { headers: headers })
+        .map(res => res.json());
+  }
+
+  // delete student
+  deleteStudent(id) {
+      return this.http.delete('api/student/' + id)
+        .map(res => res.json());
+  }
+
+  // update student
+    updateStudent(id) {
+    return this.http.put('api/student/' + id,'','')
+      .map(res => res.json());
+    }
+}
